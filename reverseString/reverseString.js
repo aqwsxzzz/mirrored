da78cfd6f192097e08ad6eb4a8330439e18dbee3
ShegoS <jbarretolarrosa@gmail.com>
2021-05-19 20:03:38 -0300

    reverseString done

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reverseStringArray = [""];
+    for (let j = str.length; j >= 0; j--) {
+        reverseStringArray.push(str[j]);
+    }
+    let finalStr = reverseStringArray.join("");
+    return finalStr;
 }
 
 module.exports = reverseString
